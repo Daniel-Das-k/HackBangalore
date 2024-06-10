0fd5acce43f2eb2eb9a3c4bf5f30dd8e6ff06f45
Daniel <danieldaskomarapu@gmail.com>
2024-06-11 00:14:13 +0530

    commited

--- creditscore.py
+++ creditscore.py
@@ -1,12 +1,12 @@
-# Load in our libraries
+
 import pandas as pd
 from sklearn.cluster import KMeans
+import pandas as pd
+import joblib
 
-dataset = pd.read_csv("/Users/danieldas/Downloads/train.csv")
+dataset = pd.read_csv("train.csv")
 
 x = dataset[["Annual_Income", "Num_of_Loan", "Monthly_Balance", "Changed_Credit_Limit"]]
-
-
 range_n_clusters = [1,2,3]
 scores = []
 
@@ -18,28 +18,14 @@ for i in range_n_clusters:
 prediction = kmeans.predict(x.head(100))
 
 print("Predicted Credit Score:", prediction)
-####### This is the DEMO of the working of the Loaded model #########
-import pandas as pd
-import joblib
 
-# Load the saved Random Forest model from the pickle file
-loaded_model = joblib.load('/Users/danieldas/Downloads/credit_scoring.pkl')
-
-# Assuming you have your input data stored in a DataFrame called 'input_data'
-# Replace this with your actual input data
+loaded_model = joblib.load('credit_scoring.pkl')
 input_data = pd.DataFrame({
     "Annual_Income": [19114.12],
     "Num_of_Loan": [4.0],
     "Monthly_Balance": [312.494089],
-    "Changed_Credit_Limit": [11.27] # Placeholder for prediction
+    "Changed_Credit_Limit": [11.27] 
 })
 
-
-# Preprocess the input data if necessary
-# Make sure it matches the preprocessing done before training the model
-
-# Predict using the loaded model
 prediction = loaded_model.predict(input_data)
-
-# Print the prediction
 print("Prediction:", prediction)
